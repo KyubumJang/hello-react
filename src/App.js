64dafbe816c75d915bc2470cac00f06e2ce8a99b
Kyubum Jang <jkb2221@gmail.com>
2021-06-29 18:01:33 +0900

    feat: study react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
+import EventPractice from './EventPractice';
+import ValidationSample from './Validation';
+import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
+class App extends Component {
+  state = {
+    color: '#000000',
+  };
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+  render() {
+    return (
+      <div>
+        {/* <MyComponent name="react" favoriteNumber={26}>
+          리액트
+        </MyComponent>
+        <Counter />
+        <Say />
+        <EventPractice />
+        <ValidationSample />
+        <div>
+          <ScrollBox
+            ref={(ref) => {
+              this.ScrollBox = ref;
+            }}
+          />
+          <button onClick={() => this.ScrollBox.scrollToBottom()}>
+            맨 밑으로
+          </button>
+        </div> 
+        <IterationSample />
+        */}
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
