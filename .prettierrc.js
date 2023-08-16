17166f71a91f0d603266615e770a19ee8b820003
Lawrene Jang <jkb2221@gmail.com>
2023-08-16 18:58:25 +0900

    🧹 chore: renew react project using creat-react-app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  semi: true,
+  tabWidth: 2,
+  printWidth: 80,
+};
